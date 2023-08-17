79f567fb067fdedc036cb59cc271654290d452bc
faikxsahin <faikxsahin@outlook.com>
2023-08-17 21:56:34 +0300

    Constrained the movement and modified the Main Camera

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -6,6 +6,8 @@ public class PlayerControls : MonoBehaviour
 {
 
     [SerializeField] float controlSpeed = 28f;
+    [SerializeField] float xRange = 10f;
+    [SerializeField] float yRange = 7f;
 
     void Update()
     {
@@ -14,13 +16,15 @@ public class PlayerControls : MonoBehaviour
 
         // Change horizontal position
         float xOffSet = xThrow * Time.deltaTime * controlSpeed;
-        float newXPos = transform.localPosition.x + xOffSet;
+        float rawXPos = transform.localPosition.x + xOffSet;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
 
         // Change vertical position
         float yOffSet = yThrow * Time.deltaTime * controlSpeed;
-        float newYPos = transform.localPosition.y + yOffSet;
+        float rawYPos = transform.localPosition.y + yOffSet;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
 
-        transform.localPosition = new Vector3(newXPos, newYPos, transform.localPosition.z);
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
     }
 
 }
